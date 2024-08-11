432df9a4ca158b8fac6011d28e34045050661aaf
Marek Belski <marek.belski@gmail.com>
2024-08-11 16:41:47 -0700

    Adds orb receiver animation scene.

--- scenes/Interactables/door_trigger.gd
+++ scenes/Interactables/door_trigger.gd
@@ -1,13 +1,22 @@
 class_name DoorTrigger3D
 extends Node3D
-
-
 signal doors_triggered
 
 @export var doors : Array[Door3D]
 @export var stay_on : bool = false
 var uses : int = 0
-var toggled : bool = false
+
+var light_orbs : Array 
+var toggled : bool
+
+func _updated_light_orbs():
+	var has_orbs = !light_orbs.is_empty()
+	if has_orbs != toggled:
+		toggled = has_orbs
+		if toggled:
+			trigger_on()
+		else:
+			trigger_off()
 
 func _unlock_and_open_doors():
 	for door in doors:
@@ -20,16 +29,20 @@ func _close_and_lock_doors():
 		door.lock()
 
 func trigger_on():
+	$OrbReceiver.orb_entered()
 	_unlock_and_open_doors()
 	uses += 1
 	doors_triggered.emit()
 
 func trigger_off():
-	if not stay_on:
-		_close_and_lock_doors()
-
+	if stay_on: return
+	$OrbReceiver.orb_exited()
+	_close_and_lock_doors()
+	
 func _on_light_trigger_body_entered(body):
-	trigger_on()
+	light_orbs.append(body)
+	_updated_light_orbs()
 
 func _on_light_trigger_body_exited(body):
-	trigger_off()
+	light_orbs.erase(body)
+	_updated_light_orbs()
---
+++ scenes/Interactables/orb_receiver.gd
@@ -0,0 +1,18 @@
+extends Node3D
+
+const GLOW_UP_ANIMATION = &"GLOW_UP"
+const GLOW_DOWN_ANIMATION = &"GLOW_DOWN"
+
+@onready var animation_player : AnimationPlayer = $AnimationPlayer
+
+func orb_entered():
+	var current_position = animation_player.get_current_animation_position()
+	var remaining_time = animation_player.get_current_animation_length() - current_position
+	animation_player.play(GLOW_UP_ANIMATION)
+	animation_player.seek(remaining_time)
+
+func orb_exited():
+	var current_position = animation_player.get_current_animation_position()
+	var remaining_time = animation_player.get_current_animation_length() - current_position
+	animation_player.play(GLOW_DOWN_ANIMATION)
+	animation_player.seek(remaining_time)
