9598f4931de78be92438a2880597867cc0ce75ef
Marek Belski <marek.belski@gmail.com>
2024-08-11 15:25:49 -0700

    Door triggers can close doors, too.

--- scenes/Interactables/door_trigger.gd
+++ scenes/Interactables/door_trigger.gd
@@ -5,6 +5,7 @@ extends Node3D
 signal doors_triggered
 
 @export var doors : Array[Door3D]
+@export var stay_on : bool = false
 var uses : int = 0
 var toggled : bool = false
 
@@ -13,11 +14,22 @@ func _unlock_and_open_doors():
 		door.unlock()
 		door.open()
 
-func trigger():
+func _close_and_lock_doors():
+	for door in doors:
+		door.close()
+		door.lock()
+
+func trigger_on():
 	_unlock_and_open_doors()
 	uses += 1
 	doors_triggered.emit()
 
-func _on_light_trigger_body_entered(body):
-	trigger()
+func trigger_off():
+	if not stay_on:
+		_close_and_lock_doors()
 
+func _on_light_trigger_body_entered(body):
+	trigger_on()
+
+func _on_light_trigger_body_exited(body):
+	trigger_off()
